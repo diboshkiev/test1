fec8073fa44d973fb958daedd858d5eab8b8da23
diboshkiev <dk@pvm.com.ua>
2023-01-06 23:18:44 +0200

    add some comments

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
+# kjhkjhkjh
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -11,6 +12,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('Hello')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
